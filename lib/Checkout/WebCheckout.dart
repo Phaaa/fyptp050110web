315b45bc08781d34aa897852fe6af8bd298bd7aa
Phaaa <philip1611@gmail.com>
2022-06-03 20:19:41 +0800

    commit

--- lib/Checkout/WebCheckout.dart
+++ lib/Checkout/WebCheckout.dart
@@ -24,113 +24,124 @@ class _WebCheckoutState extends State<WebCheckout> {
               MaterialPageRoute(builder: (context) => WebCart()));
         }),
       ),
-      body: StreamBuilder(
-        stream: retrieveCart(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.hasError) {
-            return const Center(
-              child: Text("Something went wrong"),
-            );
-          }
-          if (snapshot.hasData) {
-            var userData = snapshot.data;
-            String authAccount = userData['MfaUserId'];
-            return StreamBuilder(
-              stream: retrieveMfa(authAccount: authAccount),
-              builder: (context, AsyncSnapshot snapshot2) {
-                if (snapshot2.hasError) {
-                  return Center(
-                    child: Column(
-                      children: [
-                        Text("Something went wrong with MFA APP"),
-                        Text(snapshot2.error.toString())
-                      ],
-                    ),
-                  );
-                }
-                if (snapshot2.hasData) {
-                  var mfaUserData = snapshot2.data;
-                  String otp = mfaUserData['MfaOtp'];
-                  TextEditingController _otpController =
-                      TextEditingController();
-                  print(otp);
-                  return Container(
-                    width: 600,
-                    child: Column(children: [
-                      const Text("OTP: "),
-                      SizedBox(
-                        width: 500,
-                        child: TextField(
-                          inputFormatters: <TextInputFormatter>[
-                            LengthLimitingTextInputFormatter(6),
-                            FilteringTextInputFormatter.allow(
-                              RegExp(r'[0-9]'),
-                            ),
-                          ],
-                          keyboardType: TextInputType.number,
-                          controller: _otpController,
-                          decoration: const InputDecoration(
-                            hintText: "6-Digit OTP",
-                          ),
-                        ),
+      body: Center(
+        child: StreamBuilder(
+          stream: retrieveCart(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text("Something went wrong"),
+              );
+            }
+            if (snapshot.hasData) {
+              var userData = snapshot.data;
+              String authAccount = userData['MfaUserId'];
+              return StreamBuilder(
+                stream: retrieveMfa(authAccount: authAccount),
+                builder: (context, AsyncSnapshot snapshot2) {
+                  if (snapshot2.hasError) {
+                    return Center(
+                      child: Column(
+                        children: [
+                          const Text("Something went wrong with MFA APP"),
+                          Text(snapshot2.error.toString())
+                        ],
                       ),
-                      Container(
-                        width: 450,
-                        child: RawMaterialButton(
-                          onPressed: () {
-                            var otpInput = _otpController.text;
-                            if (otpInput == otp) {
-                              showDialog<String>(
-                                context: context,
-                                builder: (BuildContext context) => AlertDialog(
-                                  title: const Text("Success!"),
-                                  content: const Text(
-                                      "Yay! OTP validation successful."),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pushReplacement(
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    WebPayment()));
-                                      },
-                                      child: const Text("OK"),
-                                    )
-                                  ],
+                    );
+                  }
+                  if (snapshot2.hasData) {
+                    var mfaUserData = snapshot2.data;
+                    String otp = mfaUserData['MfaOtp'];
+                    TextEditingController _otpController =
+                        TextEditingController();
+                    return SizedBox(
+                      width: 450,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("OTP: "),
+                            SizedBox(
+                              width: 450,
+                              child: TextField(
+                                inputFormatters: <TextInputFormatter>[
+                                  LengthLimitingTextInputFormatter(6),
+                                  FilteringTextInputFormatter.allow(
+                                    RegExp(r'[0-9]'),
+                                  ),
+                                ],
+                                keyboardType: TextInputType.number,
+                                controller: _otpController,
+                                decoration: const InputDecoration(
+                                  hintText: "6-Digit OTP",
                                 ),
-                              );
-                            } else {
-                              showDialog<String>(
-                                context: context,
-                                builder: (BuildContext context) => AlertDialog(
-                                  title: const Text("Error"),
-                                  content: const Text("Wrong OTP"),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context, 'OK');
-                                      },
-                                      child: const Text("OK"),
-                                    )
-                                  ],
-                                ),
-                              );
-                            }
-                          },
-                          child: const Text("Authenticate Pruchase"),
-                        ),
-                      )
-                    ]),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 35,
+                            ),
+                            Container(
+                              color: Colors.amber,
+                              width: 450,
+                              child: RawMaterialButton(
+                                onPressed: () {
+                                  var otpInput = _otpController.text;
+                                  if (otpInput == otp) {
+                                    showDialog<String>(
+                                      context: context,
+                                      builder: (BuildContext context) =>
+                                          AlertDialog(
+                                        title: const Text("Success!"),
+                                        content: const Text(
+                                            "Yay! OTP validation successful."),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context)
+                                                  .pushReplacement(
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              WebPayment()));
+                                            },
+                                            child: const Text("OK"),
+                                          )
+                                        ],
+                                      ),
+                                    );
+                                  } else {
+                                    showDialog<String>(
+                                      context: context,
+                                      builder: (BuildContext context) =>
+                                          AlertDialog(
+                                        title: const Text("Error"),
+                                        content: const Text("Wrong OTP"),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context, 'OK');
+                                            },
+                                            child: const Text("OK"),
+                                          )
+                                        ],
+                                      ),
+                                    );
+                                  }
+                                },
+                                child: const Text("Authenticate Pruchase"),
+                              ),
+                            )
+                          ]),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
                   );
-                }
-                return const CircularProgressIndicator(
-                  color: Colors.red,
-                );
-              },
-            );
-          }
-          return CircularProgressIndicator(color: Colors.green);
-        },
+                },
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
   }
--- lib/Checkout/WebPayment.dart
+++ lib/Checkout/WebPayment.dart
@@ -1,7 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:fyptp050110web/Checkout/WebCheckout.dart';
+import 'package:flutter/services.dart';
 import 'package:fyptp050110web/main.dart';
 
 class WebPayment extends StatefulWidget {
@@ -14,6 +14,8 @@ class WebPayment extends StatefulWidget {
 class _WebPaymentState extends State<WebPayment> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController _cardNumberController = TextEditingController();
+    TextEditingController _cvvController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         leading: BackButton(onPressed: () {
@@ -23,8 +25,124 @@ class _WebPaymentState extends State<WebPayment> {
       ),
       body: Center(
           child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("Payment Page"),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Payment Page",
+            style: TextStyle(fontSize: 30),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Card Number: "),
+              SizedBox(
+                width: 500,
+                child: TextField(
+                  inputFormatters: <TextInputFormatter>[
+                    LengthLimitingTextInputFormatter(16),
+                    FilteringTextInputFormatter.allow(
+                      RegExp(r'[0-9]'),
+                    ),
+                  ],
+                  keyboardType: TextInputType.number,
+                  controller: _cardNumberController,
+                  decoration: const InputDecoration(
+                    hintText: "6-Digit OTP",
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Text("Expiry date: "),
+              SizedBox(
+                width: 500,
+                child: TextField(
+                  inputFormatters: <TextInputFormatter>[
+                    LengthLimitingTextInputFormatter(3),
+                    FilteringTextInputFormatter.allow(
+                      RegExp(r'[0-9]'),
+                    ),
+                  ],
+                  keyboardType: TextInputType.number,
+                  obscureText: true,
+                  controller: _cvvController,
+                  decoration: const InputDecoration(
+                    hintText: "Expiration Date",
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Text("CVV: "),
+              SizedBox(
+                width: 500,
+                child: TextField(
+                  inputFormatters: <TextInputFormatter>[
+                    LengthLimitingTextInputFormatter(3),
+                    FilteringTextInputFormatter.allow(
+                      RegExp(r'[0-9]'),
+                    ),
+                  ],
+                  keyboardType: TextInputType.number,
+                  obscureText: true,
+                  controller: _cvvController,
+                  decoration: const InputDecoration(
+                    hintText: "***",
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 40),
+          Container(
+            width: 200,
+            color: Colors.amber,
+            child: RawMaterialButton(
+              onPressed: () {
+                var userDoc = FirebaseFirestore.instance
+                    .collection("Users")
+                    .doc(FirebaseAuth.instance.currentUser!.uid);
+                userDoc.update(
+                  {
+                    'Cart': [],
+                  },
+                );
+                showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text("Thanks <3"),
+                    content: const Text(
+                        "Payment Successful! Thanks for supporting us!"),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(builder: (context) => MyApp()));
+                        },
+                        child: const Text("OK"),
+                      )
+                    ],
+                  ),
+                );
+              },
+              child: const Text("Pay"),
+            ),
+          )
         ],
       )),
     );
